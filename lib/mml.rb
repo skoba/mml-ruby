137205a5b002321276e87ae19de8d5358be35181
Shinji KOBAYASHI <skoba@moss.gr.jp>
2013-11-12 19:02:54 +0900

    implemented mmlNm

--- lib/mml.rb
+++ lib/mml.rb
@@ -1,4 +1,5 @@
 require 'builder'
+
 module MML
   MML_NAMESPACE = 'xmlns:mml="http://www.medxml.net/MML"'
 
@@ -39,6 +40,35 @@ module MML
   end
 
   class Name
+    attr_reader :repCode
+    attr_accessor :tableId, :family, :given, :middle, :fullname, :prefix, :degree
 
+    def initialize(args = {})
+      @tableId = args[:tableId]
+      self.repCode = args[:repCode]
+      @family = args[:family]
+      @given = args[:given]
+      @middle = args[:middle]
+      @fullname = args[:fullname]
+      @prefix = args[:prefix]
+      @degree = args[:degree]
+    end
+
+    def repCode=(repCode)
+      raise ArgumentError, 'repCode is nil' if repCode.nil?
+      @repCode = repCode
+    end
+
+    def to_xml
+      xb = Builder::XmlMarkup.new
+      xb.mmlNm(:Name, {'mmlNm:repCode' => @repCode, 'mmlNm:tableId' => @tableId}) do
+        xb.mmlNm :family, @family if @family
+        xb.mmlNm :middle, @middle if @middle
+        xb.mmlNm :given, @given if @given
+        xb.mmlNm :fullname, @fullname if @fullname
+        xb.mmlNm :prefix, @prefix if @prefix
+        xb.mmlNm :degree, @degree if @degree
+      end
+    end
   end
 end
--- spec/mml/name_spec.rb
+++ spec/mml/name_spec.rb
@@ -1,7 +1,67 @@
 describe MML::Name do
-  let(:name) {MML::Name.new}
+  let(:name) {MML::Name.new(repCode: 'A', tableId: 'MML0025', family: 'KOBAYASHI', given: 'Shinji', middle: 'middle', fullname: 'KOBAYASHI, Shinji', prefix: 'Dr', degree: 'MD, PhD')}
 
   it 'name should be an instance of MML::Name' do
     expect(name).to be_an_instance_of MML::Name
+  end
+
+  it 'repCode should be A' do
+    expect(name.repCode).to eq 'A'
+  end
+
+  it 'tableId should be MML0025' do
+    expect(name.tableId).to eq 'MML0025'
+  end
+
+  it 'repCode is mandatory' do
+    expect{name.repCode = nil}.to raise_error
+  end
+
+  it 'family name is KOBAYASHI' do
+    expect(name.family).to eq 'KOBAYASHI'
+  end
+
+  it 'given name is Shinji' do
+    expect(name.given).to eq 'Shinji'
+  end
+
+  it 'middle name is middle' do
+    expect(name.middle).to eq 'middle'
+  end
+
+  it 'full name is KOBAYASHI, Shinji' do
+    expect(name.fullname).to eq 'KOBAYASHI, Shinji'
+  end
+
+  it 'prefix should be Dr' do
+    expect(name.prefix).to eq 'Dr'
+  end
+
+  it 'degree is MD, PhD' do
+    expect(name.degree).to eq 'MD, PhD'
+  end
+
+  context '#to_xml' do
+    let(:xml) { name.to_xml }
+    
+    it 'mmlNm:given tag matches Shinji' do
+      expect(xml).to match '<mmlNm:given>Shinji</mmlNm:given>'
+    end
+
+    it 'mmlNm:family tag matches KOBAYASHI' do
+      expect(xml).to match '<mmlNm:family>KOBAYASHI</mmlNm:family>'
+    end
+
+    it 'mmlNm:fullname tag matches KOBAYASHI, Shinji' do
+      expect(xml).to match '<mmlNm:fullname>KOBAYASHI, Shinji</mmlNm:fullname>'
+    end
+
+    it 'mmlNm:prefix tag matches Dr' do
+      expect(xml).to match '<mmlNm:prefix>Dr</mmlNm:prefix>'
+    end
+
+    it 'mmlNm:degree tag matches MD, PhD' do
+      expect(xml).to match '<mmlNm:degree>MD, PhD</mmlNm:degree>'
+    end
   end
 end
