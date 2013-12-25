c67d0ee1076230be4a993f4a7920a87bb07a97c5
Shinji KOBAYASHI <skoba@moss.gr.jp>
2013-12-25 22:59:17 +0900

    primitive MML parser

--- lib/parser/xml_parser.rb
+++ lib/parser/xml_parser.rb
@@ -8,7 +8,52 @@ module MML
       end
 
       def parse
+        address(@xml.xpath('//mmlAd:Address'))
+      end
 
+      private
+      def address(node)
+        MML::Address.new(repCode: address_repCode(node), tableId: address_tableId(node), addressClass: address_Class(node), full: address_full(node), prefecture: address_prefecture(node), city: address_city(node), town: address_town(node), homeNumber: address_homeNumber(node), zip: address_zip(node), countryCode: address_countryCode(node))
+      end
+
+      def address_repCode(node)
+        node.xpath('@mmlAd:repCode').text
+      end
+
+      def address_tableId(node)
+        node.xpath('@mmlAd:tableId').text
+      end
+
+      def address_Class(node)
+        node.xpath('@mmlAd:addressClass').text
+      end
+
+      def address_full(node)
+        node.xpath('mmlAd:full').text
+      end
+
+      def address_prefecture(node)
+        node.xpath('mmlAd:prefecture').text
+      end
+
+      def address_city(node)
+        node.xpath('mmlAd:city').text
+      end
+
+      def address_town(node)
+        node.xpath('mmlAd:town').text
+      end
+
+      def address_homeNumber(node)
+        node.xpath('mmlAd:homeNumber').text
+      end
+
+      def address_zip(node)
+        node.xpath('mmlAd:zip').text
+      end
+
+      def address_countryCode(node)
+        node.xpath('mmlAd:countryCode').text
       end
     end
   end
--- spec/parser/xml/address_parser_spec.rb
+++ spec/parser/xml/address_parser_spec.rb
@@ -13,5 +13,41 @@ describe MML::Parser::XmlParser do
     it 'repCode is A' do
       expect(address.repCode).to eq 'A'
     end
+
+    it 'tableId is MML0025' do
+      expect(address.tableId).to eq 'MML0025'
+    end
+
+    it 'addressClass is business' do
+      expect(address.addressClass).to eq 'business'
+    end
+
+    it 'full is 506, Dept. 9, Kyoto Research Park (KRP), Awata-cho 91, Chudoji, Shimogyo-ku, Kyoto-city' do
+      expect(address.full).to eq '506, Dept. 9, Kyoto Research Park (KRP), Awata-cho 91, Chudoji, Shimogyo-ku, Kyoto-city'
+    end
+
+    it 'prefecture is Kyoto-fu' do
+      expect(address.prefecture).to eq 'Kyoto-fu'
+    end
+
+    it 'city is Kyoto-city' do
+      expect(address.city).to eq 'Kyoto-city'
+    end
+
+    it 'town is Awata-cho 91, Chudoji, Shimogyo-ku' do
+      expect(address.town).to eq 'Awata-cho 91, Chudoji, Shimogyo-ku'
+    end
+
+    it 'homeNumber is 506, Dept. 9, Kyoto Research Park (KRP)' do
+      expect(address.homeNumber).to eq '506, Dept. 9, Kyoto Research Park (KRP)'
+    end
+
+    it 'zip is 600-8815' do
+      expect(address.zip).to eq '600-8815'
+    end
+
+    it 'countryCode is JPN' do
+      expect(address.countryCode).to eq 'JPN'
+    end
   end
 end
